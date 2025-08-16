1ebfa8df2f769977f8b0b8944993bae6e4682d4f
Nine499 <14026156+Nine499@users.noreply.github.com>
2025-08-16 18:59:13 +0800

    build: 更新版本号并移除构建脚本
    - 删除了 build.ps1 文件，可能因为不再需要自动构建和推送功能
    - 更新了 pyproject.toml 中的版本号，从 "2025.08.16.094334" 改为 "2025.08.16.185851"

--- build.ps1
+++
@@ -1,8 +0,0 @@
-﻿Remove-Item "dist" -Recurse -Force
-uv run dev\version-update.py
-uv build
-
-git add .
-git commit -m "$(Get-Date -Format "yyyy-M-d_HH:mm:ss")"
-git remote add origin git@github.com:Nine499/AI-49.git
-git push
